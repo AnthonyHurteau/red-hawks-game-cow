ae6f2bea0ef69629cf829ed9708fe5339904cede
Anthony Hurteau <anthony.hurteau@radio-canada.ca>
2024-10-09 22:44:18 -0400

    Commit api

--- infrastructure-as-code/bin/red-hawks-game-cow.ts
+++ infrastructure-as-code/bin/red-hawks-game-cow.ts
@@ -2,6 +2,8 @@
 import * as cdk from "aws-cdk-lib";
 import { RedHawksGameCowStack } from "../lib/red-hawks-game-cow";
 
+require("dotenv").config();
+
 const appName = "RedHawksGameCow";
 const stackName = `stack-${process.env.AWS_REGION}-${appName}`;
 
